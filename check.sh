e5b6fecc4ce89bbe0f5e4a92a6b86b1b96fa164d
666wcy <769020367@qq.com>
2021-01-23 21:10:52 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -6,6 +6,8 @@ if [ "`ls -A $DIRECTORY`" = "" ]; then
 	mv  /qbittorrent_python_rclone/*   /upload
 	rm -rf /qbittorrent_python_rclone/
 	sudo chmod 777 /upload/ -R
+	tail -f /dev/null
 else
   echo "$DIRECTORY is not empty"
+  tail -f /dev/null
 fi		
